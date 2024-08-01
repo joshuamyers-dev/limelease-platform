4d141dd4d55d1a6a3cdb3d0438c6364a788d2053
Joshua Nissenbaum <>
2024-08-01 12:19:12 +1000

    last fix

--- limelease/services/elixir-api/lib/lime_lease/property_request/property_request_service.ex
+++ limelease/services/elixir-api/lib/lime_lease/property_request/property_request_service.ex
@@ -31,9 +31,9 @@ defmodule LimeLease.PropertyRequest.PropertyRequestService do
       end
 
     with {:ok, %PropertyRequest{} = request} <- PropertyRequestContext.create_request(args, photos) do
-        spawn(fn  ->
-          PropertyRequestCommentService.create_system_comment_for_request(request.id, Helpers.full_user_name(user), "Request created.")
-        end)
+      spawn(fn ->
+        PropertyRequestCommentService.create_system_comment_for_request(request.id, Helpers.full_user_name(user), "Request created.")
+      end)
 
       {:ok, request}
     else
@@ -45,7 +45,8 @@ defmodule LimeLease.PropertyRequest.PropertyRequestService do
   def create_request_status_screenshot(%PropertyRequest{} = request) do
     url = Application.get_env(:lime_lease, :front_end_url) <> "/requests/#{request.ticket_number}"
 
-    with {:ok, screenshot_base64} <- Req.post("#{Application.get_env(:ex_aws, :api_gateway_endpoint)}/snapshot", json: %{url: url}),
+    with {:ok, %Req.Response{body: screenshot_base64, status: 200}} <-
+           Req.post("#{Application.get_env(:ex_aws, :api_gateway_endpoint)}/snapshot", json: %{url: url}),
          {:ok, %StaticMedia{} = static_media} <- StaticMediaService.create_static_media("screenshot.png", "image/png"),
          {:ok, put_url} <- AWS.generate_presigned_put_url(static_media.s3_key, static_media.mime_type),
          {:ok, %Req.Response{status: 200}} <- Req.put(put_url, body: screenshot_base64),
