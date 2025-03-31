a38cc8dde5cba58ac9dc72092ba13614e65f596d
Joshua Nissenbaum <joshn@hotmail.com.au>
2025-04-01 10:31:19 +1100

    ignore files

--- sst.config.ts
+++ sst.config.ts
@@ -117,7 +117,7 @@ export default $config({
         ],
       },
       health: {
-        command: ["CMD-SHELL", "curl -f http://127.0.0.1:80/login || exit 1"],
+        command: ["CMD-SHELL", "curl -f http://127.0.0.1:3000/login || exit 1"],
         startPeriod: "120 seconds",
         timeout: "10 seconds",
         interval: "60 seconds",
